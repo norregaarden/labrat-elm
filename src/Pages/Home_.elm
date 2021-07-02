672f2d861b5de388a5f5680506ec697b0c6a9d62
Norregaarden <jna.jelling@gmail.com>
2021-07-02 18:29:15 +0200

    PWA

--- src/Pages/Home_.elm
+++ src/Pages/Home_.elm
@@ -2,8 +2,8 @@ module Pages.Home_ exposing (view)
 
 -- elm-spa server
 
-import Element exposing (Element)
-import UI exposing (h)
+import Element exposing (Element, fill, image, paddingXY, width)
+import UI exposing (h, s)
 import View exposing (View)
 
 
@@ -21,7 +21,11 @@ init =
 
 vis: List (Element msg)
 vis =
-  h 1 "Welcome, lab rat." |> List.singleton
+  [ h 1 "Welcome, lab rat."
+  , image [width fill, paddingXY 0 (s 3) ]
+    { src = "/images/rottefyr.png"
+    , description = "rottefyr" }
+  ]
 
 
 view : View msg
